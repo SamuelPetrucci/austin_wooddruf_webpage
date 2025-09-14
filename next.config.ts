d67a56b453275e3ae2b5a844f2fa2806b56b22b5
SamuelPetrucci <sam.pet8803@yahoo.com>
2025-09-14 18:28:46 -0400

    Fix build errors: Remove deprecated swcMinify and problematic optimizeCss

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Performance optimizations
-  swcMinify: true,
-  compress: true,
-  
   // Build optimizations
   experimental: {
-    optimizeCss: true,
     optimizePackageImports: ['@heroicons/react'],
   },
   
   // Image optimization
   images: {
     formats: ['image/webp', 'image/avif'],
-  },
-  
-  // Reduce bundle size
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
-        },
-      };
-    }
-    return config;
   },
 };
 
